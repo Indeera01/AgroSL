e0a37a61f3193e5527d0de20c3a0d7ce9c7da73e
Pavalakumaran Vithurshan <pavalakumaran.21@cse.mrt.ac.lk>
2024-07-06 23:07:35 +0530

    Side Bar Draft

---
+++ src/Components/Side_Drawer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Drawer, List, Divider, ListItem, ListItemButton, ListItemIcon, ListItemText ,Box,Toolbar} from '@mui/material';
+import {StorefrontOutlined,ShoppingCartOutlined,FeedbackOutlined,HistoryOutlined} from '@mui/icons-material';
+const drawerWidth = 240;
+
+
+const Side_Drawer = () => {
+  return (
+    <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box',backgroudColor:"#98BC74" },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+        <List>
+            {[
+                { name: 'Items', icon: <StorefrontOutlined /> },
+                { name: 'Cart', icon: <ShoppingCartOutlined /> },
+                { name: 'Complaint', icon: <FeedbackOutlined /> },
+                { name: 'History', icon: <HistoryOutlined /> }
+            ].map((item, index) => (
+                <ListItem key={item.name} disablePadding>
+                <ListItemButton>
+                    <ListItemIcon>
+                    {item.icon}
+                    </ListItemIcon>
+                    <ListItemText primary={item.name} />
+                </ListItemButton>
+                </ListItem>
+            ))}
+            </List>
+        </Box>
+      </Drawer>
+  )
+}
+
+export default Side_Drawer
