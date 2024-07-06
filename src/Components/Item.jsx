0e6664c701f196655e5e2a9165579fb2e93942ef
Pavalakumaran Vithurshan <pavalakumaran.21@cse.mrt.ac.lk>
2024-07-06 23:03:09 +0530

    Item card draft

---
+++ src/Components/Item.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import backgroundImage from '../assets/background.jpg';
+
+const Item = () => {
+  return (
+    <Card sx={{ minWidth: {md:200,xs:300},maxWidth:250 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="100"
+        image={backgroundImage}
+      />
+      <CardContent sx={{ p: 0 }}>
+        <Typography gutterBottom variant="h6" component="div">
+          LKR 200
+        </Typography>
+        <Typography gutterBottom variant="h7" component="div">
+          Quantity : 1kg
+        </Typography>
+        <Typography gutterBottom variant="h7" component="div">
+          Seller : Sample Seller
+        </Typography>
+      </CardContent>
+      <CardActions sx={{justifyContent:'space-between'}}>
+        <Button size="small" variant="contained" color="primary">Buy</Button>
+        <Button size="small" variant="contained" color="primary">Add To Cart</Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default Item
