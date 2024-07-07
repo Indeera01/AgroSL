09776d8e63c6cf14cb54d6531be0a2dad49a6902
Pavalakumaran Vithurshan <pavalakumaran.21@cse.mrt.ac.lk>
2024-07-07 14:04:00 +0530

    testing sample routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import Sign_Up from './Pages/Sign_Up';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sign_Up from './Pages/Sign_Up_Buyer';
 import Sign_In from './Pages/Sign_In';
 import Navigation_Bar from './Components/Navigation_Bar';
 import Side_Drawer from './Components/Side_Drawer';
-import React from 'react';
-import {Box} from '@mui/material';
-import Items from './Components/Items'
+import Success from './Pages/Success';
+import Layout from './Layout';
+import Items from './Components/Items';
+import User_Profile from './Pages/User_Profile';
 
 const App = () => {
   return (
-    <Box>
-        <Navigation_Bar/>
-        <Items/>
-    </Box>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/Sign_Up" element={<Sign_Up />} />
+        <Route path="/Sign_In" element={<Sign_In />} />
+        <Route path="/Success" element={<Success />} />
+        <Route path="/ItemsList/:id" element={<Layout />} />
+        <Route path="/user/:id" element={<User_Profile />} />
+      </Routes>
+    </Router>
   );
 };
 
-export default App
+export default App;
