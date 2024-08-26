544fe8cf8b161f8afe3e7e1ca574f19c4c402f5a
udesh p.v.d <damitha.21@cse.mrt.ac.lk>
2024-08-26 17:29:31 +0530

    updated Sign_in page to verify sellers and buyers

--- src/Pages/Sign_In.jsx
+++ src/Pages/Sign_In.jsx
@@ -1,42 +1,70 @@
-import React, { useState } from 'react';
-import { Box, Typography, TextField, Button, Container, Link } from '@mui/material';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../../firebase'; 
-import { useNavigate } from 'react-router-dom';
-import backgroundImage from '../assets/pxfuel.jpg'; 
+import React, { useState } from "react";
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Container,
+  Link,
+} from "@mui/material";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import backgroundImage from "../assets/pxfuel.jpg";
+import axios from "axios";
 
 const Sign_In = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleSignIn = async () => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      console.log('User signed in:', email);
+      console.log("User signed in:", email);
       const currentUser = auth.currentUser;
 
-      navigate(`/Home/${currentUser.uid}`);
+      const response = await axios.get(
+        `http://localhost:5001/get_user/${currentUser.uid}`
+      );
+      const userType = response.data.user_type;
+      console.log("User type:", userType);
+      if (userType === "buyer") {
+        navigate(`/Home/${currentUser.uid}`);
+      } else if (userType === "seller") {
+        navigate(`/seller_dashboard/${currentUser.uid}`);
+      } else {
+        alert("User type not recognized");
+      }
     } catch (error) {
-      console.error('Error signing in:', error);
-      alert('Error signing in');
+      console.error("Error signing in:", error);
+      alert("Error signing in");
     }
   };
 
   return (
-    <Box width={'100%'} height={'100vh'} sx={{backgroundImage:`url(${backgroundImage})`,backgroundRepeat:'no-Repeat',backgroundSize:'cover'}} position={'absolute'}>
+    <Box
+      width={"100%"}
+      height={"100vh"}
+      sx={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundRepeat: "no-Repeat",
+        backgroundSize: "cover",
+      }}
+      position={"absolute"}
+    >
       <Container
         maxWidth="xs"
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
           p: 4,
           borderRadius: 6,
           boxShadow: 3,
-          backgroundColor: 'rgba(255, 255, 255, 0.7)',
-          marginTop:'10%',
+          backgroundColor: "rgba(255, 255, 255, 0.7)",
+          marginTop: "10%",
         }}
       >
         <Typography variant="h4" fontFamily="open-sans" gutterBottom mb={2}>
@@ -66,12 +94,15 @@ const Sign_In = () => {
           variant="contained"
           color="primary"
           onClick={handleSignIn}
-          sx={{ mt: 2, width: '40%', fontSize: '16px' }}
+          sx={{ mt: 2, width: "40%", fontSize: "16px" }}
         >
           Sign In
         </Button>
         <Typography variant="body2" sx={{ mt: 2 }}>
-          Don’t have an account? <Link color="secondary" href="/Sign_Up_Buyer">Sign Up</Link>
+          Don’t have an account?{" "}
+          <Link color="secondary" href="/Sign_Up_Buyer">
+            Sign Up
+          </Link>
         </Typography>
       </Container>
     </Box>
