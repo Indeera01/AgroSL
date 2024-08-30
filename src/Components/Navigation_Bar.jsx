554f3f3347fdb1a9485177e27641d1e06dc67085
Indeera Udayanga <129371871+Indeera01@users.noreply.github.com>
2024-08-30 11:36:44 +0530

    cart notifications, chat icon

--- src/Components/Navigation_Bar.jsx
+++ src/Components/Navigation_Bar.jsx
@@ -1,33 +1,83 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import { auth } from '../../firebase'; 
-import { useNavigate } from 'react-router-dom';
-import User_Profile from '../Pages/User_Profile';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import NotificationIcon from '@mui/icons-material/Notifications';
-import { Badge } from '@mui/material';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import User_Profile from "../Pages/User_Profile";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import NotificationIcon from "@mui/icons-material/Notifications";
+import { Badge } from "@mui/material";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import ChatIcon from "@mui/icons-material/Chat";
 
-
-const pages = ['Become a Seller','Category', 'Contact us', 'About'];
-const settings = ['Profile', 'Logout'];
+const pages = ["Become a Seller", "Category", "Contact us", "About"];
+const settings = ["Profile", "Logout"];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
   const [showUserProfile, setShowUserProfile] = React.useState(false);
   const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [cart, setCart] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Fetch logged-in user details from Firebase Authentication
+    const currentUser = auth.currentUser;
+    if (currentUser) {
+      axios
+        .get(`http://localhost:5001/users/${currentUser.uid}`)
+        .then((res) => {
+          setUser(res.data);
+          setUpdatedUser(res.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setLoading(false);
+        });
+    }
+  }, []);
+
+  useEffect(() => {
+    // Fetch cart items when user data is available
+    if (user?.user_id) {
+      const fetchCartItems = async (userID) => {
+        try {
+          const response = await axios.get(
+            `http://localhost:5001/cart/${userID}`
+          );
+          setCart(response.data);
+          console.log("Cart items:", response.data);
+        } catch (err) {
+          // Handle 404 or any other errors gracefully
+          if (err.response && err.response.status === 404) {
+            setCart([]); // Set cartItems to empty array if 404 error
+          } else {
+            setError(err.message);
+          }
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchCartItems(user.user_id); // Call the function with user.uid
+    }
+  }, [user]);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -38,34 +88,37 @@ function ResponsiveAppBar() {
   };
 
   const handleCloseNavMenu = (page) => {
-    if(page === "Become a Seller"){
-      navigate('/Sign_Up_Seller');
-    }else if(page === "Category"){
-      navigate('/Category');
-    }else if(page === "Contact us"){
-      navigate('/Contact_us');
-    }else if(page === "About"){
-      navigate('/About');
+    if (page === "Become a Seller") {
+      navigate("/Sign_Up_Seller");
+    } else if (page === "Category") {
+      navigate("/Category");
+    } else if (page === "Contact us") {
+      navigate("/Contact_us");
+    } else if (page === "About") {
+      navigate("/About");
     }
     setAnchorElNav(null);
   };
 
   const handleCloseUserMenu = (setting) => {
-    if (setting === 'Profile') {
+    if (setting === "Profile") {
       handleProfilePopUp();
-    } else if (setting === 'Logout') {
+    } else if (setting === "Logout") {
       handleLogout();
     }
     setAnchorElUser(null);
   };
 
   const handleLogout = () => {
-    auth.signOut().then(() => {
-      console.log("User signed out");
-      navigate("/Sign_in");
-    }).catch((error) => {
-      console.error("Error signing out:", error);
-    });
+    auth
+      .signOut()
+      .then(() => {
+        console.log("User signed out");
+        navigate("/Sign_in");
+      })
+      .catch((error) => {
+        console.error("Error signing out:", error);
+      });
   };
 
   const handleProfilePopUp = () => {
@@ -78,7 +131,7 @@ function ResponsiveAppBar() {
       <AppBar position="static">
         <Container maxWidth="xl">
           <Toolbar disableGutters>
-            <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
             <Typography
               variant="h6"
               noWrap
@@ -86,18 +139,18 @@ function ResponsiveAppBar() {
               href="#"
               sx={{
                 mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                fontFamily: 'monospace',
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
                 fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
               }}
             >
               AgroSL
             </Typography>
 
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
               <IconButton
                 size="large"
                 aria-label="account of current user"
@@ -112,28 +165,28 @@ function ResponsiveAppBar() {
                 id="menu-appbar"
                 anchorEl={anchorElNav}
                 anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
+                  vertical: "bottom",
+                  horizontal: "left",
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
+                  vertical: "top",
+                  horizontal: "left",
                 }}
                 open={Boolean(anchorElNav)}
                 onClose={handleCloseNavMenu}
                 sx={{
-                  display: { xs: 'block', md: 'none' },
+                  display: { xs: "block", md: "none" },
                 }}
               >
                 {pages.map((page) => (
-                  <MenuItem key={page} onClick={()=>handleCloseNavMenu(page)}>
+                  <MenuItem key={page} onClick={() => handleCloseNavMenu(page)}>
                     <Typography textAlign="center">{page}</Typography>
                   </MenuItem>
                 ))}
               </Menu>
             </Box>
-            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
             <Typography
               variant="h5"
               noWrap
@@ -141,39 +194,50 @@ function ResponsiveAppBar() {
               href="#"
               sx={{
                 mr: 2,
-                display: { xs: 'flex', md: 'none' },
+                display: { xs: "flex", md: "none" },
                 flexGrow: 1,
-                fontFamily: 'monospace',
+                fontFamily: "monospace",
                 fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
               }}
             >
               LOGO
             </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
               {pages.map((page) => (
                 <Button
                   key={page}
-                  onClick={()=>handleCloseNavMenu(page)}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
+                  onClick={() => handleCloseNavMenu(page)}
+                  sx={{ my: 2, color: "white", display: "block" }}
                 >
                   {page}
                 </Button>
               ))}
             </Box>
 
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: '20px' }} mr={10}>
-            <Badge badgeContent={4} color="action">
-              <ShoppingCartIcon fontSize='large' onClick={()=>navigate('/cart')} />
-            </Badge>
-            <Badge badgeContent={4} color="action">
-              <NotificationIcon fontSize='large' onClick={()=>navigate('/notifications')} />
-            </Badge>
-          </Box>
-
-
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                gap: "20px",
+              }}
+              mr={10}
+            >
+              <Badge badgeContent={cart.length} color="action">
+                <ShoppingCartIcon
+                  fontSize="large"
+                  onClick={() => navigate("/cart")}
+                />
+              </Badge>
+              <Badge badgeContent={0} color="action">
+                <ChatIcon
+                  fontSize="large"
+                  onClick={() => navigate("/notifications")}
+                />
+              </Badge>
+            </Box>
 
             <Box sx={{ flexGrow: 0 }}>
               <Tooltip title="Open settings">
@@ -182,23 +246,26 @@ function ResponsiveAppBar() {
                 </IconButton>
               </Tooltip>
               <Menu
-                sx={{ mt: '45px' }}
+                sx={{ mt: "45px" }}
                 id="menu-appbar"
                 anchorEl={anchorElUser}
                 anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 open={Boolean(anchorElUser)}
                 onClose={() => setAnchorElUser(null)}
               >
                 {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={() => handleCloseUserMenu(setting)}>
+                  <MenuItem
+                    key={setting}
+                    onClick={() => handleCloseUserMenu(setting)}
+                  >
                     <Typography textAlign="center">{setting}</Typography>
                   </MenuItem>
                 ))}
@@ -208,9 +275,7 @@ function ResponsiveAppBar() {
         </Container>
       </AppBar>
       {showUserProfile && (
-        <User_Profile
-          onClose={() => setShowUserProfile(false)}
-        />
+        <User_Profile onClose={() => setShowUserProfile(false)} />
       )}
     </>
   );
