46f9e4eb71c905603ff18750f547e2bb509c75db
udesh p.v.d <damitha.21@cse.mrt.ac.lk>
2024-09-28 11:40:12 +0530

    firbase configs

--- firebase.js
+++ firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
