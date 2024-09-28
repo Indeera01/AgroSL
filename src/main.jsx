781ba59b8676834eb30d0eba2676d43069a18886
Indeera Udayanga <129371871+Indeera01@users.noreply.github.com>
2024-09-28 10:32:52 +0530

    speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#4B8412',
-      },
-      secondary: {
-        main: '#000000',
-      },
+  palette: {
+    primary: {
+      main: "#4B8412",
     },
-  });
+    secondary: {
+      main: "#000000",
+    },
+  },
+});
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
+      <CssBaseline />
+      <App />
+      <SpeedInsights />
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
