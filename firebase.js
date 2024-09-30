7562ceca87ab319d48c281269da567d6cb506470
udesh p.v.d <damitha.21@cse.mrt.ac.lk>
2024-09-30 13:25:57 +0530

    added the changes done to firebase

--- firebase.js
+++ firebase.js
@@ -17,5 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
+export { storage };
