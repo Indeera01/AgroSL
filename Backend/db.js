bd8de34da859bc5c6a49bcc622ba5f91bc359d35
Pavalakumaran Vithurshan <pavalakumaran.21@cse.mrt.ac.lk>
2024-10-08 21:34:53 +0530

    try to host node js backend in vercel

--- Backend/db.js
+++ Backend/db.js
@@ -1,12 +1,16 @@
 const Pool = require("pg").Pool;
 require("dotenv").config({ path: "../.env" });
 
+// const pool = new Pool({
+//   user: "postgres",
+//   password: process.env.DB_PASSWORD,
+//   host: "localhost",
+//   port: 5432,
+//   database: "AgroSl_Latest",
+// });
+
 const pool = new Pool({
-  user: "postgres",
-  password: process.env.DB_PASSWORD,
-  host: "localhost",
-  port: 5432,
-  database: "AgroSL",
+  connectionString: process.env.POSTGRES_URL,
 });
 
 module.exports = pool;
