7bb7c69984d5755ffaa0cbd4692dd2f08970d19b
udesh p.v.d <damitha.21@cse.mrt.ac.lk>
2024-10-10 14:09:42 +0530

    made responsive but still seller dashboard some components are not responsive

--- src/Pages/Seller_Dashboard.jsx
+++ src/Pages/Seller_Dashboard.jsx
@@ -96,25 +96,273 @@ export default function Seller_Dashboard() {
     navigate(path);
   };
 
+  // return (
+  //   <Box
+  //     sx={{
+  //       backgroundColor: "#e6ffe6",
+  //       height: "100%",
+  //       minHeight: "100vh",
+  //     }}
+  //   >
+  //     <Navigation_Bar_Seller />
+  //     <Box sx={{ flex: 1, padding: "20px", display: "flex" }}>
+  //       <Box
+  //         sx={{
+  //           width: "250px",
+  //           backgroundColor: "#F4F7F6",
+  //           borderRadius: "10px",
+  //           padding: "20px",
+  //           marginRight: "20px",
+  //           boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+  //           minHeight: "100vh",
+  //         }}
+  //       >
+  //         <List>
+  //           {/* Post Items Button */}
+  //           <ListItem
+  //             button
+  //             onClick={() => handleNavigation(`/add_item/${id}`)}
+  //             sx={{
+  //               borderRadius: "8px",
+  //               padding: "10px",
+  //               marginBottom: "10px",
+  //               backgroundColor: "#f5f5f5",
+  //               transition: "background-color 0.3s, box-shadow 0.3s",
+  //               "&:hover": {
+  //                 backgroundColor: "#d1e7ff",
+  //                 boxShadow: "0 4px 12px rgba(0, 123, 255, 0.2)",
+  //               },
+  //             }}
+  //           >
+  //             <ListItemText primary="Post Items" />
+  //           </ListItem>
+
+  //           {/* View Orders Button */}
+  //           <ListItem
+  //             button
+  //             onClick={() => handleNavigation(`/sellerdashboard/${id}/orders`)}
+  //             sx={{
+  //               borderRadius: "8px",
+  //               padding: "10px",
+  //               marginBottom: "10px",
+  //               backgroundColor: "#f5f5f5",
+  //               transition: "background-color 0.3s, box-shadow 0.3s",
+  //               "&:hover": {
+  //                 backgroundColor: "#d1e7ff",
+  //                 boxShadow: "0 4px 12px rgba(0, 123, 255, 0.2)",
+  //               },
+  //             }}
+  //           >
+  //             <ListItemText primary="View Orders" />
+  //           </ListItem>
+
+  //           {/* View Inventory Button */}
+  //           <ListItem
+  //             button
+  //             onClick={() => handleNavigation(`/inventory`)}
+  //             sx={{
+  //               borderRadius: "8px",
+  //               padding: "10px",
+  //               marginBottom: "10px",
+  //               backgroundColor: "#f5f5f5",
+  //               transition: "background-color 0.3s, box-shadow 0.3s",
+  //               "&:hover": {
+  //                 backgroundColor: "#d1e7ff",
+  //                 boxShadow: "0 4px 12px rgba(0, 123, 255, 0.2)",
+  //               },
+  //             }}
+  //           >
+  //             <ListItemText primary="View Inventory" />
+  //           </ListItem>
+
+  //           <ListItem
+  //             button
+  //             onClick={() =>
+  //               handleNavigation(`/sellerdashboard/${id}/complaints`)
+  //             }
+  //             sx={{
+  //               borderRadius: "8px",
+  //               padding: "10px",
+  //               marginBottom: "10px",
+  //               backgroundColor: "#f5f5f5",
+  //               transition: "background-color 0.3s, box-shadow 0.3s",
+  //               "&:hover": {
+  //                 backgroundColor: "#d1e7ff",
+  //                 boxShadow: "0 4px 12px rgba(0, 123, 255, 0.2)",
+  //               },
+  //             }}
+  //           >
+  //             <ListItemText primary="View Complaints" />
+  //           </ListItem>
+  //         </List>
+  //       </Box>
+
+  //       {/* Main Content */}
+  //       <Box
+  //         sx={{
+  //           flex: 1,
+  //           padding: "20px",
+  //           borderRadius: "10px",
+  //           backgroundColor: "#fff",
+  //           boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+  //         }}
+  //       >
+  //         {loading ? (
+  //           <CircularProgress />
+  //         ) : error ? (
+  //           <Typography color="error">{error}</Typography>
+  //         ) : (
+  //           <>
+  //             <Typography
+  //               variant="h5"
+  //               gutterBottom
+  //               sx={{ fontWeight: "600", color: "#333" }}
+  //             >
+  //               Welcome, {user.first_name} {user.last_name}
+  //             </Typography>
+  //             <Box
+  //               sx={{
+  //                 marginBottom: "20px",
+  //                 display: "flex",
+  //                 flexDirection: "row",
+  //               }}
+  //             >
+  //               <Card
+  //                 sx={{
+  //                   width: "25%",
+  //                   margin: "20px",
+  //                   backgroundColor: "#f5f5f5",
+  //                   height: "440px",
+  //                 }}
+  //               >
+  //                 <CardMedia
+  //                   component="img"
+  //                   sx={{
+  //                     width: "100%", // Make sure the image takes the full width of the parent Box
+  //                     height: "250px", // Maintain aspect ratio based on the image's width
+  //                     objectFit: "cover",
+  //                   }}
+  //                   image={
+  //                     user.image_url
+  //                       ? user.image_url
+  //                       : "https://firebasestorage.googleapis.com/v0/b/agrosl-7abb2.appspot.com/o/items%2Fdefault_user.jpg?alt=media&token=9d2d5193-73f0-4ba2-9776-06b91c4ca354"
+  //                   }
+  //                   title={user.first_name}
+  //                 />
+  //                 <CardContent>
+  //                   <Typography gutterBottom variant="h5" component="div">
+  //                     {store}
+  //                   </Typography>
+  //                   <Typography variant="h6">
+  //                     Owner : {user.first_name} {user.last_name}
+  //                   </Typography>
+  //                   <Typography variant="h6">Email : {user.email}</Typography>
+  //                   <Typography variant="h6">
+  //                     Mobile Number : {user.mobile_number}
+  //                   </Typography>
+  //                 </CardContent>
+  //               </Card>
+  //               <Box
+  //                 sx={{
+  //                   width: "40%",
+  //                   display: "flex",
+  //                   flexDirection: "column",
+  //                   justifyContent: "center",
+  //                   alignContent: "center",
+  //                 }}
+  //               >
+  //                 <Card
+  //                   sx={{
+  //                     padding: "10px",
+  //                     backgroundColor: "#f5f5f5",
+  //                     height: "200px",
+  //                     margin: "20px",
+  //                   }}
+  //                 >
+  //                   <CardContent>
+  //                     <Typography variant="h6" color="primary">
+  //                       Number of Orders
+  //                     </Typography>
+  //                     <Typography variant="h4">{stats.orders}</Typography>
+  //                   </CardContent>
+  //                 </Card>
+
+  //                 <Card
+  //                   sx={{
+  //                     padding: "10px",
+  //                     backgroundColor: "#f5f5f5",
+  //                     height: "200px",
+  //                     margin: "20px",
+  //                   }}
+  //                 >
+  //                   <CardContent>
+  //                     <Typography variant="h6" color="primary">
+  //                       Number of Items
+  //                     </Typography>
+  //                     <Typography variant="h4">{stats.items}</Typography>
+  //                   </CardContent>
+  //                 </Card>
+  //               </Box>
+  //             </Box>
+
+  //             {/* Statistics Section */}
+
+  //             <Reports_seller />
+  //           </>
+  //         )}
+  //       </Box>
+  //     </Box>
+  //     <Box
+  //       position="fixed"
+  //       bottom="12%"
+  //       right="4%"
+  //       zIndex="1000"
+  //       bgcolor="white"
+  //       padding={2}
+  //       borderRadius="50%"
+  //       boxShadow="0px 8px 10px rgba(0, 0, 0, 0.8)"
+  //       onClick={() => handleChatMessagePress()}
+  //       sx={{
+  //         cursor: "pointer",
+  //         transition: "transform 0.2s ease-in-out",
+  //         "&:hover": {
+  //           transform: "scale(1.2)",
+  //         },
+  //       }}
+  //     >
+  //       <Badge badgeContent={0} color="error">
+  //         <ChatIcon fontSize="large" color="black" />
+  //       </Badge>
+  //     </Box>
+  //   </Box>
+  // );
   return (
     <Box
       sx={{
         backgroundColor: "#e6ffe6",
-        height: "100%",
         minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
       }}
     >
       <Navigation_Bar_Seller />
-      <Box sx={{ flex: 1, padding: "20px", display: "flex" }}>
+      <Box
+        sx={{
+          flex: 1,
+          padding: { xs: "10px", sm: "20px" },
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+        }}
+      >
         <Box
           sx={{
-            width: "250px",
+            width: { xs: "100%", md: "250px" },
             backgroundColor: "#F4F7F6",
             borderRadius: "10px",
-            padding: "20px",
-            marginRight: "20px",
+            padding: { xs: "10px", sm: "20px" },
+            marginBottom: { xs: "20px", md: "0" },
+            marginRight: { xs: "0", md: "20px" },
             boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-            minHeight: "100vh",
           }}
         >
           <List>
@@ -175,25 +423,6 @@ export default function Seller_Dashboard() {
               <ListItemText primary="View Inventory" />
             </ListItem>
 
-            {/* View Reports Button */}
-            {/* <ListItem
-              button
-              onClick={() => handleNavigation("/reports_seller")}
-              sx={{
-                borderRadius: "8px",
-                padding: "10px",
-                marginBottom: "10px",
-                backgroundColor: "#f5f5f5",
-                transition: "background-color 0.3s, box-shadow 0.3s",
-                "&:hover": {
-                  backgroundColor: "#d1e7ff",
-                  boxShadow: "0 4px 12px rgba(0, 123, 255, 0.2)",
-                },
-              }}
-            >
-              <ListItemText primary="View Reports" />
-            </ListItem> */}
-
             {/* View Complaints Button */}
             <ListItem
               button
@@ -221,7 +450,7 @@ export default function Seller_Dashboard() {
         <Box
           sx={{
             flex: 1,
-            padding: "20px",
+            padding: { xs: "10px", sm: "20px" },
             borderRadius: "10px",
             backgroundColor: "#fff",
             boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
@@ -240,16 +469,16 @@ export default function Seller_Dashboard() {
               >
                 Welcome, {user.first_name} {user.last_name}
               </Typography>
-              <Box
+              {/* <Box
                 sx={{
                   marginBottom: "20px",
                   display: "flex",
-                  flexDirection: "row",
+                  flexDirection: { xs: "column", md: "row" },
                 }}
               >
                 <Card
                   sx={{
-                    width: "25%",
+                    width: { xs: "100%", md: "25%" },
                     margin: "20px",
                     backgroundColor: "#f5f5f5",
                     height: "440px",
@@ -258,8 +487,8 @@ export default function Seller_Dashboard() {
                   <CardMedia
                     component="img"
                     sx={{
-                      width: "100%", // Make sure the image takes the full width of the parent Box
-                      height: "250px", // Maintain aspect ratio based on the image's width
+                      width: "100%",
+                      height: "250px",
                       objectFit: "cover",
                     }}
                     image={
@@ -281,14 +510,58 @@ export default function Seller_Dashboard() {
                       Mobile Number : {user.mobile_number}
                     </Typography>
                   </CardContent>
+                </Card> */}
+              <Box
+                sx={{
+                  marginBottom: "20px",
+                  display: "flex",
+                  flexDirection: { xs: "column", md: "row" },
+                  justifyContent: "space-between", // Add space between items in larger screens
+                }}
+              >
+                <Card
+                  sx={{
+                    width: { xs: "100%", md: "25%" }, // Full width on small screens, 25% on medium and larger
+                    margin: { xs: "10px 0", md: "20px" }, // Adjust margin for small screens
+                    backgroundColor: "#f5f5f5",
+                    height: "auto", // Use auto height to accommodate content
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      width: "100%",
+                      height: { xs: "200px", md: "250px" }, // Responsive height
+                      objectFit: "cover",
+                    }}
+                    image={
+                      user.image_url
+                        ? user.image_url
+                        : "https://firebasestorage.googleapis.com/v0/b/agrosl-7abb2.appspot.com/o/items%2Fdefault_user.jpg?alt=media&token=9d2d5193-73f0-4ba2-9776-06b91c4ca354"
+                    }
+                    title={user.first_name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {store}
+                    </Typography>
+                    <Typography variant="h6">
+                      Owner: {user.first_name} {user.last_name}
+                    </Typography>
+                    <Typography variant="h6">Email: {user.email}</Typography>
+                    <Typography variant="h6">
+                      Mobile Number: {user.mobile_number}
+                    </Typography>
+                  </CardContent>
                 </Card>
+
                 <Box
                   sx={{
-                    width: "40%",
+                    width: { xs: "100%", md: "40%" },
                     display: "flex",
                     flexDirection: "column",
                     justifyContent: "center",
-                    alignContent: "center",
+                    alignItems: "center",
                   }}
                 >
                   <Card
@@ -297,6 +570,7 @@ export default function Seller_Dashboard() {
                       backgroundColor: "#f5f5f5",
                       height: "200px",
                       margin: "20px",
+                      width: "100%",
                     }}
                   >
                     <CardContent>
@@ -313,6 +587,7 @@ export default function Seller_Dashboard() {
                       backgroundColor: "#f5f5f5",
                       height: "200px",
                       margin: "20px",
+                      width: "100%",
                     }}
                   >
                     <CardContent>
@@ -323,45 +598,16 @@ export default function Seller_Dashboard() {
                     </CardContent>
                   </Card>
                 </Box>
-
-                {/* <Box
-                  sx={{
-                    padding: "15px",
-                    borderRadius: "8px",
-                    backgroundColor: "#F9F9F9",
-                    boxShadow: "0 2px 8px rgba(0, 0, 0, 0.05)",
-                  }}
-                >
-                  <Typography
-                    variant="body1"
-                    gutterBottom
-                    sx={{ fontWeight: "500", color: "#555" }}
-                  >
-                    Email:{" "}
-                    <span style={{ fontWeight: "400", color: "#333" }}>
-                      {user.email}
-                    </span>
-                  </Typography>
-                  <Typography
-                    variant="body1"
-                    gutterBottom
-                    sx={{ fontWeight: "500", color: "#555" }}
-                  >
-                    Mobile Number:{" "}
-                    <span style={{ fontWeight: "400", color: "#333" }}>
-                      {user.mobile_number}
-                    </span>
-                  </Typography>
-                </Box> */}
               </Box>
 
               {/* Statistics Section */}
-
               <Reports_seller />
             </>
           )}
         </Box>
       </Box>
+
+      {/* Chat Icon */}
       <Box
         position="fixed"
         bottom="12%"
